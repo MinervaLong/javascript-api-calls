33eaa7f6a7d99dfbe9df2d4f57b77f47e1c0e3ef
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-04-08 11:38:28 +0200

    Add call to API with promises

--- js/main.js
+++ js/main.js
@@ -37,3 +37,17 @@ jokeBtn.addEventListener('click',getJoke);
 
 let weather = document.getElementById('weather');
 
+// Call the weather API using fetch with promises
+
+fetch('https://www.el-tiempo.net/api/json/v2/provincias/08') // The fetch request is sent
+.then((response) => { 
+    return response.json() // Return the response in JSON
+})
+.then((data) => { // Passes the JSON so it can be showed on screen
+    const {today} = data // Desestructuring data
+    const {p} = today
+    weather.textContent = `El tiempo hoy en Barcelona Provincia: ${p}`;
+})
+.catch((error) => { // In case there is an error
+    console.log(error)
+})
